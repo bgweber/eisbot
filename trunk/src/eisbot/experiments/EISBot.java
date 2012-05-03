080b0e94d892dd30acf3cd83fa27df85884f85ac
bgweber@gmail.com <bgweber@gmail.com@66b6f02c-354a-1c2a-ebb4-0a8d61143fb2>
2012-05-03 16:53:17 +0000

    Modified default agent configuration

--- trunk/src/eisbot/experiments/EISBot.java
+++ trunk/src/eisbot/experiments/EISBot.java
@@ -7,18 +7,8 @@ import eisbot.abl.ABLStarCraftBot;
  * Note: run with VM arg -Xmx512M
  */ 
 public class EISBot { 
- 
-	public static void main(String[] args) {
   
-		System.setProperty("ABLBotName", "EISBot");   
-  		 
-		System.setProperty("GoalFormulator.Enable", "true");
-		System.setProperty("GoalFormulator.PlanningWindowSize", "15");
-		System.setProperty("OpponentPredictor.Enable", "true");
-		System.setProperty("OpponentPredictor.PredictionWindowSize", "10");
-		System.setProperty("GDA.Enable", "true");
-		System.setProperty("GDA.DiscrepancyPeriod", "" + (24*30));
-    
+	public static void main(String[] args) {
 		ABLStarCraftBot.main(args);  
 	}   
 }
